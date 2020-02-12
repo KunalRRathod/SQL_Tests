f93941248761e4e48ceb5ab74e423b11bf32b3b7
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2020-02-12 10:38:38 +0530

    Define SQL Triggers

---
+++ db3.sql
@@ -0,0 +1,21 @@
+create database db2;
+use db2;
+CREATE TABLE Student(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+          
+   PRIMARY KEY (ID),
+);
+insert into student values ("9","Bucky", "1", "Vienna"); 
+select * from Student;
+commit;
+start transaction;
+savepoint a;
+rollback;
+savepoint b;
+rollback to a;
+ALTER TABLE STUDENT ADD Foreign key  (AGE) references student(ID);
+delimiter //
+CREATE TRIGGER nameadd BEFORE INSERT ON Student FOR EACH ROW IF NEW.age < 0 THEN SET NEW.age = 0; END IF;
